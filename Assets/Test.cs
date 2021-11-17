1496898cbf0c929db58714779f8d554f152ed1c7
Keijiro Takahashi <keijiro@gmail.com>
2021-11-17 21:48:57 +0900

    VideoWriter implementation

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -1,29 +1,49 @@
 using UnityEngine;
+using Unity.Collections;
+using Unity.Collections.LowLevel.Unsafe;
+using IntPtr = System.IntPtr;
+using IEnumerator = System.Collections.IEnumerator;
 
 sealed class Test : MonoBehaviour
 {
     [System.Runtime.InteropServices.DllImport("VideoWriter")]
-    static extern void VideoWriter_Start();
+    static extern void VideoWriter_Start(string filePath, int width, int height);
 
     [System.Runtime.InteropServices.DllImport("VideoWriter")]
-    static extern void VideoWriter_Update();
+    static extern void VideoWriter_Update(IntPtr pointer, uint size);
 
     [System.Runtime.InteropServices.DllImport("VideoWriter")]
     static extern void VideoWriter_End();
 
-    System.Collections.IEnumerator Start()
+    NativeArray<Color32> _buffer;
+
+    void Start()
     {
+        _buffer = new NativeArray<Color32>
+          (1920 * 1080, Allocator.Persistent,
+           NativeArrayOptions.UninitializedMemory);
+
         Debug.Log("Start");
-        VideoWriter_Start();
+        VideoWriter_Start("Test.mp4", 1920, 1080);
+    }
 
-        for (var i = 0; i < 30; i++)
-        {
-            Debug.Log($"Update ({i})");
-            yield return new WaitForSeconds(1.0f / 30);
-            VideoWriter_Update();
-        }
+    unsafe void Update()
+    {
+        if (Time.frameCount % 4 != 0) return;
 
+        var p = (Color32)Color.HSVToRGB(Time.time % 1, 1, 1);
+        for (var i = 0; i < 1920*1080; i++)
+            _buffer[i]  = p;
+
+        Debug.Log("Update");
+        var ptr = NativeArrayUnsafeUtility.GetUnsafeReadOnlyPtr(_buffer);
+        VideoWriter_Update((IntPtr)ptr, 1920 * 1080 * 4);
+    }
+
+    void OnDestroy()
+    {
         Debug.Log("End");
         VideoWriter_End();
+        _buffer.Dispose();
     }
 }
--- Plugin/Plugin.m
+++ Plugin/Plugin.m
@@ -1,103 +1,94 @@
-#import <Foundation/Foundation.h>
 #import <AVFoundation/AVFoundation.h>
-#import <AVFoundation/AVAssetWriter.h>
 
+// Internal objects
 static AVAssetWriter* _writer;
 static AVAssetWriterInput* _writerInput;
 static AVAssetWriterInputPixelBufferAdaptor* _bufferAdaptor;
 static CFTimeInterval _startTime;
-static int _counter;
 
-extern void VideoWriter_Start(void)
+extern void VideoWriter_Start(const char* filePath, int width, int height)
 {
-    NSURL* fileURL = [NSURL fileURLWithPath:@"test.mp4"];
+    if (_writer)
+    {
+        NSLog(@"Recording has already initiated.");
+        return;
+    }
+    
+    // Asset writer setup
+    NSURL* filePathURL =
+      [NSURL fileURLWithPath:[NSString stringWithUTF8String:filePath]];
 
     _writer =
-      [[AVAssetWriter alloc] initWithURL: fileURL
+      [[AVAssetWriter alloc] initWithURL: filePathURL
                                 fileType: AVFileTypeQuickTimeMovie
-                                   error: nil];
+                                   error: NULL];
 
+    // Asset writer input setup
     NSDictionary* settings =
       @{ AVVideoCodecKey: AVVideoCodecTypeH264,
-         AVVideoWidthKey: @(1920),
-        AVVideoHeightKey: @(1080) };
+         AVVideoWidthKey: @(width),
+        AVVideoHeightKey: @(height) };
 
     _writerInput =
-      [AVAssetWriterInput assetWriterInputWithMediaType: AVMediaTypeVideo
-                                         outputSettings: settings];
-
+      [[AVAssetWriterInput assetWriterInputWithMediaType: AVMediaTypeVideo
+                                          outputSettings: settings] retain];
     _writerInput.expectsMediaDataInRealTime = true;
-    
+
+    [_writer addInput:_writerInput];
+
+    // Pixel buffer adaptor setup
     NSDictionary* attribs =
       @{ (NSString*)kCVPixelBufferPixelFormatTypeKey: @(kCVPixelFormatType_32BGRA),
-                   (NSString*)kCVPixelBufferWidthKey: @(1920),
-                  (NSString*)kCVPixelBufferHeightKey: @(1080) };
+                   (NSString*)kCVPixelBufferWidthKey: @(width),
+                  (NSString*)kCVPixelBufferHeightKey: @(height) };
 
     _bufferAdaptor =
-      [AVAssetWriterInputPixelBufferAdaptor
-        assetWriterInputPixelBufferAdaptorWithAssetWriterInput: _writerInput
-                                   sourcePixelBufferAttributes: attribs];
+      [[AVAssetWriterInputPixelBufferAdaptor
+         assetWriterInputPixelBufferAdaptorWithAssetWriterInput: _writerInput
+                                    sourcePixelBufferAttributes: attribs] retain];
     
-    [_writer addInput:_writerInput];
+    // Recording start
+    _startTime = CACurrentMediaTime();
     [_writer startWriting];
     [_writer startSessionAtSourceTime:kCMTimeZero];
-    _startTime = CACurrentMediaTime();
-    _counter = 0;
-    
-    [_writerInput retain];
-    [_bufferAdaptor retain];
 }
 
-extern void VideoWriter_Update(void)
+extern void VideoWriter_Update(const void* source, uint32_t size)
 {
-    if (!_writerInput.isReadyForMoreMediaData)
-    {
-        NSLog(@"_writerInput.isReadyForMoreMediaData is false");
-        return;
-    }
+    if (!_writerInput.isReadyForMoreMediaData) return;
     
-    if (_bufferAdaptor.pixelBufferPool == NULL)
-    {
-        NSLog(@"pixelBufferPool is null");
-        return;
-    }
-    
+    // Buffer allocation
     CVPixelBufferRef buffer;
     CVReturn ret = CVPixelBufferPoolCreatePixelBuffer
       (NULL, _bufferAdaptor.pixelBufferPool, &buffer);
-    
-    if (ret != kCVReturnSuccess)
-    {
-        NSLog(@"CVPixelBufferPoolCreatePixelBuffer failed");
-        return;
-    }
-    
+    if (ret != kCVReturnSuccess) return;
+
     CVPixelBufferLockBaseAddress(buffer, 0);
     void* pointer = CVPixelBufferGetBaseAddress(buffer);
-    size_t stride = CVPixelBufferGetBytesPerRow(buffer);
-    uint32_t fill = ((_counter * 8) & 0xff) | 0xff000000;
-    
-    for (int y = 0; y < 1080; y++)
-    {
-        uint32_t* array = pointer + stride * y;
-        for (int x = 0; x < 1920; x++)
-        {
-            array[x] = fill;
-        }
-    }
-    
+    size_t buffer_size = CVPixelBufferGetDataSize(buffer);
+
+    // Current time
     CFTimeInterval frameTime = CACurrentMediaTime() - _startTime;
-    CMTime preTime = CMTimeMakeWithSeconds(frameTime, 240);
+    CMTime intTime = CMTimeMakeWithSeconds(frameTime, 240);
+
+    // Buffer update
+    memcpy(pointer, source, MIN(size, buffer_size));
     
-    [_bufferAdaptor appendPixelBuffer:buffer withPresentationTime:preTime];
+    // Buffer submission
+    [_bufferAdaptor appendPixelBuffer:buffer withPresentationTime:intTime];
     CVPixelBufferUnlockBaseAddress(buffer, 0);
-    
-    _counter++;
 }
 
 extern void VideoWriter_End(void)
 {
     [_writerInput markAsFinished];
     [_writer finishWritingWithCompletionHandler:^{ NSLog(@"Done"); }];
+
     [_writer release];
+    [_writerInput release];
+    [_bufferAdaptor release];
+    
+    _writer = NULL;
+    _writerInput = NULL;
+    _bufferAdaptor = NULL;
 }
